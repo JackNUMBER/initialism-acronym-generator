fdba88370599902c1647885f2d8a6079b96655ba
JackNUMBER <antoine.cdrt@gmail.com>
2016-12-28 17:22:20 +0100

    roman numeral

--- generate/initialism.php
+++ generate/initialism.php
@@ -12,19 +12,6 @@ while (($line = fgets($file)) !== false) {
 }
 fclose($file);
 
-// preset roman numbers
-$roman_numeral = [
-    'I'    => 1,
-    'II'   => 2,
-    'III'  => 3,
-    'IV'   => 4,
-    'V'    => 5,
-    'VI'   => 6,
-    'VII'  => 7,
-    'VIII' => 8,
-    'IX'   => 9,
-];
-/*
 // sample
 $names = [
     'SCAR : Squadra Corse Alfa Romeo',
@@ -37,16 +24,41 @@ $names = [
     'Black',
     'Tenku 2',
 ];
-*/
-echo "\n";
+
+// Roman/Arabic numerals relation
+$roman_arabic_numeral_relation = [
+    'II'   => '2',
+    'III'  => '3',
+    'IV'   => '4',
+    'V'    => '5',
+    'VI'   => '6',
+    'VII'  => '7',
+    'VIII' => '8',
+    'IX'   => '9',
+];
+
+// regex parttern
+$roman_arabic_numeral_patterns = implode('|', array_keys($roman_arabic_numeral_relation));
+
+// replace pattern
+$roman_numeral = [];
+$arabic_numeral = [];
+
+foreach ($roman_arabic_numeral_relation as $roman => $arabic) {
+    $roman_numeral[] = '/\b(' . $roman . ')\b/';
+    $arabic_numeral[] = $arabic;
+}
 
 // cli table render
 $mask = "| %1.1s | %-8.8s | %-40.40s | %-5.5s |\n";
+$mask = "| %1.1s | %-15.30s | %-40.40s | %-5.5s |\n";
+echo "\n";
 printf($mask, '', 'abbr', 'name', 'words');
 printf($mask, '', '', '', '', '');
 
 // process games names
 foreach ($names as $name) {
+    $abbr = [];
     $ignore = false;
     $words_count = substr_count($name, ' ') + 1; // str_word_count() ignore numbers
 
@@ -54,11 +66,17 @@ foreach ($names as $name) {
         $ignore = true;
     }
 
-    $name_clean;
+    // there is Roman numerals
+    if (preg_match_all('/\b(' . $roman_arabic_numeral_patterns . ')\b/', $name, $m)) {
+        $temp = preg_replace($roman_numeral, $arabic_numeral, $name);
 
-    // take first letter of each word, keep complete numbers, keep ":"
-    if (preg_match_all('/\b([a-zA-Z]|\d+|:)/', strtoupper($name), $m)) {
-        $abbr = str_replace(':', ' ', implode('', $m[1]));
+        if (preg_match_all('/\b([a-zA-Z]|\d+|:)/', strtoupper($temp), $m)) {
+            $abbr[] = str_replace(':', ' ', implode('', $m[1]));
+        }
+    }
+
+    if (preg_match_all('/\b(' . $roman_arabic_numeral_patterns . '|[a-zA-Z]|\d+|:)/', strtoupper($name), $m)) {
+        $abbr[] = str_replace(':', ' ', implode('', $m[1]));
     }
 
     // cli ignore render
@@ -68,7 +86,7 @@ foreach ($names as $name) {
         $ignore_display = 'x';
     }
 
-    printf($mask, $ignore_display, $abbr, $name, $words_count);
+    printf($mask, $ignore_display, '(' . count($abbr) . ') ' . implode(', ', $abbr), $name, $words_count);
 }
 
 echo "\n";
